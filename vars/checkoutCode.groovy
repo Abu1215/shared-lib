356455207a34bf142af3a536b754d821bcd8a6b7
Abu bakkar Khan <119516014+Abu1215@users.noreply.github.com>
2024-02-08 16:37:21 +0530

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -4,3 +4,8 @@ def call(String repoUrl, String branch){
    sh "git checkout ${branch}"
    return workingDir
 }
+
+def repoUrl = "https://github.com/Abu1215/demo-deploy.git"
+def branch = "master" // Or whatever branch you want to checkout
+
+def workingDir = call(repoUrl, branch)
